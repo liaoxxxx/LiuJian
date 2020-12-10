6137c418aa8dbb695c1c5b7660baf4783956ac4c
liaoyj <993501756@qq.com>
2020-12-10 18:04:19 +0800

    1.首页数据api

--- goApi/app/models/systemGroupData.go
+++ goApi/app/models/systemGroupData.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"fmt"
 	orm "goApi/app/models/database"
 	"gorm.io/gorm"
@@ -15,14 +16,29 @@ type SystemGroupData struct {
 	gorm.Model
 }
 
+type SystemGroupDataValue struct {
+	ID     int64  `gorm:"primaryKey;autoIncrement:true"`
+	Gid    int64  `json:"gid"`
+	Value  string `json:"value"`
+	Sort   int32  `json:"sort"`
+	Status int8   `json:"status"`
+}
+
 func (SystemGroupData) TableName() string {
 	return "eb_system_group_data"
 }
 
 //列表
-func (systemGroupData *SystemGroupData) GetList(gid int64) (sysGroupDataList []SystemGroupData, err error) {
+func (systemGroupData *SystemGroupData) GetList(gid int64) (data map[string]interface{}, err error) {
+	sysGroupDataList := make([]SystemGroupData, 3)
 	err = orm.Eloquent.Where(&SystemGroupData{Gid: gid}).Find(&sysGroupDataList).Error
-	fmt.Println(sysGroupDataList)
+
+	for i := 0; i < len(sysGroupDataList); i++ {
+		var byt = []byte(sysGroupDataList[i].Value)
+		_ = json.Unmarshal(byt, &data)
+
+		fmt.Println(data)
+	}
 	return
 
 }
