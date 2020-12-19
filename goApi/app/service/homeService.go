b5748ee26bf6f19383cdca5976b2ecec7c24b085
liaoxx <993501756@qq.com>
2020-12-20 01:49:47 +0800

    1.首页数据api

--- goApi/app/service/homeService.go
+++ goApi/app/service/homeService.go
@@ -16,11 +16,16 @@ func GetHomeMobileData(uid int64) *helper.Response {
 	bannerList, _ := sysGroup.GetDataByConfigName("routine_home_banner")
 	dataMap["BannerList"] = bannerList
 	//回收种类
-
+	recycleCate, _ := sysGroup.GetDataByConfigName("user_client_home_recycle_category")
+	dataMap["RecycleCate"] = recycleCate
 	//滚动通知
 	rollNotice, _ := sysGroup.GetDataByConfigName("routine_home_roll_news")
 	dataMap["RollNoticeList"] = rollNotice
 	//当前城市
+
+	//滚动通知
+	anotherOption, _ := sysGroup.GetDataByConfigName("routine_home_roll_news")
+	dataMap["AnotherOption"] = anotherOption
 
 	resp.Code = http.StatusBadRequest
 	resp.Data = dataMap
