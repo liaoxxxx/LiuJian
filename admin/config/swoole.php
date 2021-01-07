5e64dbeedd8fa8f36aa61d4bc6f30df43c5a98c2
liaoyj <993501756@qq.com>
2021-01-07 17:32:12 +0800

    1. 修改admin 为swoole服务器

---
+++ admin/config/swoole.php
@@ -0,0 +1,12 @@
+<?php
+return [
+
+    'server'=>[
+
+        'host'=>'0.0.0.0',
+        'port'=>'9555',
+        'options'=>[
+            'pid_file'=>__DIR__ . '/../runtime/swoole_server_pid'
+        ]
+    ]
+];
